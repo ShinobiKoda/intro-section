ec7461ff8f1c48af223331d6a3f8d60f1abc636a
shinobiKoda <sirp2804@gmail.com>
2024-08-22 19:24:37 +0100

    completed sidebar link list

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const sidebar = document.querySelector(".sidebar");
 const overlay = document.querySelector(".overlay");
+const link_title = document.querySelectorAll(".link-title");
+const arrow_img = document.querySelectorAll(".arrow-img");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   sidebar.classList.toggle("show");
   overlay.classList.toggle("active");
+});
+
+link_title.forEach((title) => {
+  const list = title.nextElementSibling;
+  const arrow_img = title.querySelector(".arrow-img");
+
+  title.addEventListener("click", () => {
+    list.classList.toggle("display");
+    arrow_img.classList.toggle("rotate");
+  });
 });
